5ae93ec880851105322dc40d460448459bac012f
“adarshh247” <adarsh9394@gmail.com>
2026-03-16 18:43:44 +0530

    fix: corrected index.html path to dist folder

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/{*splat}", (req, res) => {
   console.log(`[HTTP] Root route accessed by: ${req.ip}`);
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const rooms = new Map();
